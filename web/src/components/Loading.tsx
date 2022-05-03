12fc39c96429a330b1ad965a17f750c79df87b59
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2022-05-03 16:07:14 -0300

    feat: loading's component layout

--- web/src/components/Loading.tsx
+++ web/src/components/Loading.tsx
@@ -2,8 +2,8 @@ import { CircleNotch } from "phosphor-react";
 
 export function Loading() {
   return (
-    <div>
-      <CircleNotch weight="bold" className="h-4 w-4" />
+    <div className="w-6 h-6 flex items-center justify-center overflow-hidden">
+      <CircleNotch weight="bold" className="h-4 w-4 animate-spin" />
     </div>
   )
 }
