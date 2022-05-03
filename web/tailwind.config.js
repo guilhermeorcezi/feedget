ab68fd6c29f571d757b455598d4d8c90551490a1
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2022-05-03 15:56:27 -0300

    feat: widget form component

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,9 +5,16 @@ module.exports = {
       colors: {
         brand: {
           500: '#8257e6',
+          300: '#8257e6'
         }
+      },
+      borderRadius:{
+        md: '4px'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar')
+  ],
 }
