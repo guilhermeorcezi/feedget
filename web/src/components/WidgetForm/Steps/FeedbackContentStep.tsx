a882abba055ad4eb0782f23740fd55bb0af6a622
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2022-05-03 16:25:49 -0300

    feat: screenshot button component

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
-import { ArrowLeft, Camera } from "phosphor-react";
+import { ArrowLeft  } from "phosphor-react";
+import { useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from "./ScreenshotButton";
 
 interface FeedbackContentStepProps{
   feedbackType: FeedbackType;
@@ -10,6 +12,7 @@ interface FeedbackContentStepProps{
 export function FeedbackContentStep({
   feedbackType,
   onFeedbackRestartRequested }: FeedbackContentStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null);
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
   return (
@@ -32,18 +35,19 @@ export function FeedbackContentStep({
       
       <form className="my-4 w-full">
         <textarea
-        className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
-        placeholder="Conte com detalhes o que está acontecendo..."
+          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+          placeholder="Conte com detalhes o que está acontecendo..."
         />
 
         <footer className="flex gap-2 mt-2">
-          <button type="button" className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500">
-            <Camera className="w-6 h-6" />
-          </button>
+          <ScreenshotButton
+            onScreenshotTook={setScreenshot}
+            screenshot={screenshot}
+          />
 
           <button
-          type="submit"
-          className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
           >
             Enviar feedback
           </button>
--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -50,7 +50,7 @@ export function WidgetForm() {
       )}
 
       <footer className="text-xs text-neutral-400">
-        Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
+        Feito com ♥ por <a className="underline underline-offset-2" href="https://guilhermeorcezi.github.io">Guilherme Orcezi</a>
       </footer>
     </div>
   )
