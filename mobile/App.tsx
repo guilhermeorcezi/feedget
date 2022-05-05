b51856b4800973afccb457263bef4ff406271971
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2022-05-05 14:34:53 -0300

    feat: expo's mobile init config

---
+++ mobile/App.tsx
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import Widget from './src/components/Widget';
+import { theme } from './src/theme';
+import { useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter'
+import AppLoading from 'expo-app-loading';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Widget/>
+      <StatusBar
+        backgroundColor="transparent"
+        translucent
+        style="auto"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.background,
+  },
+});
---
+++ mobile/src/components/Widget/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Widget: React.FC = () => {
+  return <View />;
+}
+
+export default Widget;
